d2e2b9beea35810a6ca975f27c039e5fdcae84b4
Joe Osborne <reachjoe.o@gmail.com>
2015-09-03 16:51:33 +0100

    Generic growable RingBuffer
    A simple, generic ring buffer which grows when capacity is reached.

---
+++ RingBuffer/RingBuffer.cs
@@ -0,0 +1,98 @@
+#region License
+/* Copyright 2014 Joe Osborne
+ * 
+ * This file is part of RingBuffer.
+ *
+ *  RingBuffer is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  RingBuffer is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RingBuffer. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+using System;
+
+namespace RingBuffer {
+    /// <summary>
+    /// A generic ring buffer. Grows when
+    /// </summary>
+    /// <typeparam name="T">The type of data stored in the buffer</typeparam>
+    public class RingBuffer<T> {
+
+        private int head = 0;
+        private int tail = 0;
+        private int size = 0;
+
+        private T[] buffer;
+
+        /// <summary>
+        /// The total number of elements the buffer can store (grows).
+        /// </summary>
+        public int Capacity { get { return buffer.Length; } }
+
+        /// <summary>
+        /// The number of elements currently contained in the buffer.
+        /// </summary>
+        public int Size { get { return size; } }
+
+        //==Buffer Access======================================================
+        /// <summary>
+        /// Retrieve the next item from the buffer.
+        /// </summary>
+        /// <returns>The oldest item added to the buffer.</returns>
+        public T Get() {
+            if(size == 0) throw new System.InvalidOperationException("Buffer is empty.");
+            T _item = buffer[head];
+            head = (head + 1) % Capacity;
+            size--;
+            return _item;
+        }
+
+        /// <summary>
+        /// Adds an item to the end of the buffer.
+        /// </summary>
+        /// <param name="toAdd">The item to be added.</param>
+        public void Put(T toAdd) {
+            if(tail == head && size != 0) {
+                T[] _newArray = new T[buffer.Length * 2];
+                for(int i = 0; i < Capacity; i++) {
+                    _newArray[i] = buffer[i];
+                }
+                buffer = _newArray;
+                tail = (head + size) % Capacity;
+                addToBuffer(toAdd);
+            }
+            else {
+                addToBuffer(toAdd);
+            }
+        }
+
+        // So we can be DRY
+        private void addToBuffer(T toAdd) {
+            buffer[tail] = toAdd;
+            tail = (tail + 1) % Capacity;
+            size++;
+        }
+
+        //==Constructors=======================================================
+        /// <summary>
+        /// Creates a new RingBuffer with capacity of 4.
+        /// </summary>
+        public RingBuffer() : this(4) { }
+
+        /// <summary>
+        /// Creates a new RingBuffer.
+        /// </summary>
+        /// <param name="startCapacity">The initial capacity of the buffer.</param>
+        public RingBuffer(int startCapacity) {
+            buffer = new T[startCapacity];
+        }
+    }
+}
---
+++ RingBufferTests/RingBufferTests.cs
@@ -0,0 +1,82 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RingBuffer;
+
+namespace RingBufferTests {
+    [TestClass]
+    public class RingBufferTests {
+
+        private int iterations = 1000;
+
+        /// <summary>
+        /// Ensures that size is correctly augmented when items are added.
+        /// </summary>
+        [TestMethod()]
+        public void PutIncrementsSize() {
+            RingBuffer<int> _buffer = new RingBuffer<int>();
+            for(int i = 0; i < iterations; i++) {
+                int _tmp = i;
+                _buffer.Put(_tmp);
+                Assert.AreEqual(i + 1, _buffer.Size, "Size is not equal to number of elements added.");
+            }
+        }
+
+        /// <summary>
+        /// Ensures that size is correctly adjusted when elements are removed.
+        /// </summary>
+        [TestMethod()]
+        public void GetDecrementsSize() {
+            RingBuffer<int> _buffer = new RingBuffer<int>();
+            populateBuffer(iterations, _buffer);
+            for(int i = iterations; i > 0; i--) {
+                int _tmp = _buffer.Get();
+                Assert.AreEqual(i-1, _buffer.Size, "Size does not reflect the correct number of removed elements.");
+            }
+        }
+
+        /// <summary>
+        /// Ensures that capacity expands as needed
+        /// </summary>
+        [TestMethod()]
+        public void CapacityExpands() {
+            int _startCapacity = 12;
+            RingBuffer<double> _testBuffer = new RingBuffer<double>(_startCapacity);
+            for(int i = 0; i < _startCapacity + 1; i++) {
+                _testBuffer.Put((double)i);
+            }
+            Assert.AreEqual(_startCapacity * 2, _testBuffer.Capacity, "Capacity not expanded");
+            Assert.AreEqual(_startCapacity + 1, _testBuffer.Size, "incorrect number of elements");
+        }
+
+        /// <summary>
+        /// Ensures that head/tail move properly by testing the value of data
+        /// returned by get.
+        /// </summary>
+        [TestMethod()]
+        public void RetrievedInCorrectOrder() {
+            int _iterations = 10;
+            RingBuffer<int> _buffer = new RingBuffer<int>();
+            populateBuffer(iterations, _buffer);
+            for(int i = 0; i < _iterations; i++) {
+                int _tmp = _buffer.Get();
+                Assert.AreEqual(i, _tmp, "Incorrect Sequence");
+            }
+        }
+
+        /// <summary>
+        /// Ensures that an exception is thrown when Get() is called on an
+        /// empty buffer.
+        /// </summary>
+        [TestMethod(), ExpectedException(typeof(InvalidOperationException))]
+        public void ThrowsError_GetEmpty() {
+            RingBuffer<byte> buffer = new RingBuffer<byte>();
+            byte _tmp = buffer.Get();
+        }
+
+        private void populateBuffer(int elements, RingBuffer<int> buffer) {
+            for(int i = 0; i < elements; i++) {
+                buffer.Put(i);
+            }
+        }
+    }
+}
